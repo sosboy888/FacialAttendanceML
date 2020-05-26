51f238e9da497e7293cca7fc42bc9fa732f962b7
Swastik Sharma <swastiksharma88@gmail.com>
2020-05-26 17:18:03 +0530

    Added a recognizer

---
+++ DataRecognize.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 26 16:58:01 2020
+
+@author: sosboy888
+"""
+
+import face_recognition
+import docopt
+from sklearn import svm
+import os
+import cv2
+import joblib
+def faceRecognize(imgPath):
+    classifier=joblib.load("trainedData.sos")
+    img=face_recognition.load_image_file(imgPath)
+    faces=face_recognition.face_locations(img)
+    image=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    for face in faces:
+        top_left=(face[3],face[0])
+        bottom_right=(face[1],face[2])
+        color=[255,0,0]
+        cv2.rectangle(image,top_left,bottom_right,color,1)
+    number=len(faces)
+    print("Number of faces detected="+str(number))
+    for i in range(number):
+        imgEncoding=face_recognition.face_encodings(img)[i]
+        id=classifier.predict([imgEncoding])
+        print(*id)
+    cv2.imshow("Faces",image)
+    cv2.waitKey(0)
+    cv2.destroyWindow("Faces")
+faceRecognize("1.png")
