9de7cfc8b8dde17d9acc9ddf55f5ac24236e6014
Swastik Sharma <swastiksharma88@gmail.com>
2020-05-26 17:23:11 +0530

    Added image files

--- DataRecognize.py
+++ DataRecognize.py
@@ -11,23 +11,25 @@ from sklearn import svm
 import os
 import cv2
 import joblib
-def faceRecognize(imgPath):
+class Recognizer:
     classifier=joblib.load("trainedData.sos")
-    img=face_recognition.load_image_file(imgPath)
-    faces=face_recognition.face_locations(img)
-    image=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
-    for face in faces:
-        top_left=(face[3],face[0])
-        bottom_right=(face[1],face[2])
-        color=[255,0,0]
-        cv2.rectangle(image,top_left,bottom_right,color,1)
-    number=len(faces)
-    print("Number of faces detected="+str(number))
-    for i in range(number):
-        imgEncoding=face_recognition.face_encodings(img)[i]
-        id=classifier.predict([imgEncoding])
-        print(*id)
-    cv2.imshow("Faces",image)
-    cv2.waitKey(0)
-    cv2.destroyWindow("Faces")
-faceRecognize("1.png")
+    def __init__(self):
+        pass
+    def faceRecognize(self,imgPath):
+        img=face_recognition.load_image_file(imgPath)
+        faces=face_recognition.face_locations(img)
+        image=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+        for face in faces:
+            top_left=(face[3],face[0])
+            bottom_right=(face[1],face[2])
+            color=[255,0,0]
+            cv2.rectangle(image,top_left,bottom_right,color,1)
+        number=len(faces)
+        print("Number of faces detected="+str(number))
+        for i in range(number):
+            imgEncoding=face_recognition.face_encodings(img)[i]
+            id=self.classifier.predict([imgEncoding])
+            print(*id)
+        cv2.imshow("Faces",image)
+        cv2.waitKey(0)
+        cv2.destroyWindow("Faces")
